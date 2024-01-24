3a505fa4012c260062ce8e5a4d214a8678d77658
Hubert Klamann <hubert.klamann1@gmail.com>
2024-01-24 17:21:41 +0100

    Interactive-Background

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import React from 'react';
 import { MainPage } from './components/MainPage';
 import './sass/style.scss';
 // import { Header } from './Components/Header';
 // import { Footer } from './Components/Footer';
+import ParticlesBackground from './components/ParticlesBackground/ParticlesBackground';
 
 function App() {
   return (
-    <div className="container">
-      {/* <Header /> */}
-      <MainPage />
-      {/* <Footer /> */}
-    </div>
+    <>
+      <ParticlesBackground />
+      <div className="container">
+        {/* <Header /> */}
+        <MainPage />
+        {/* <Footer /> */}
+      </div>
+    </>
   );
 }
 
