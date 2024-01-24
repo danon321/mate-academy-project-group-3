fe53dca454f436017534c3418d4fd9e1fb873de8
Hubert Klamann <hubert.klamann1@gmail.com>
2024-01-24 13:15:46 +0100

    MPG3-6,7,8-Calculator-Our-Services

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { MainPage } from './components/MainPage';
 import './sass/style.scss';
+// import { Header } from './Components/Header';
+// import { Footer } from './Components/Footer';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      {/* <Header /> */}
+      <MainPage />
+      {/* <Footer /> */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,3 @@
+export function Footer() {
+  return <header className="footer">Footer</header>;
+}
