419cdea7c2b126057b8fd5ec60572c01e7fa84b7
Hubert Klamann <hubert.klamann1@gmail.com>
2024-02-11 20:01:45 +0100

    Footer component + our service fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
 import { MainPage } from './components/MainPage';
 import './sass/style.scss';
-// import { Header } from './Components/Header';
-// import { Footer } from './Components/Footer';
 import ParticlesBackground from './components/ParticlesBackground/ParticlesBackground';
+import { Footer } from './components/Footer/Footer';
 
 function App() {
   return (
     <>
       <ParticlesBackground />
       <div className="container">
-        {/* <Header /> */}
         <MainPage />
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </>
   );
