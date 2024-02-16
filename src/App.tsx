f840d9f45aab5282303f441c25d9acc0abd5a953
Kamil <majcherczak.kamil@protonmail.com>
2024-02-16 20:46:31 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './sass/style.scss';
 import ParticlesBackground from './components/ParticlesBackground/ParticlesBackground';
 import { Footer } from './components/Footer/Footer';
 import { Banner } from './components/Banner/Banner';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <>
       <ParticlesBackground />
       <div className="container">
+        <Header />
         <Banner />
         <MainPage />
         <Footer />
